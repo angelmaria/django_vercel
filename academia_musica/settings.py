1dc0ebab1550fcc11465efa607fbb9ee526804d7
angelmaria <angelmaria75@gmail.com>
2024-07-17 01:51:49 +0200

    comento el host para vercel hasta que lo arregle

--- academia_musica/settings.py
+++ academia_musica/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['.vercel.app']
 
 
 
